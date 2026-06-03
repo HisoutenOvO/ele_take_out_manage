0626060399f99ee965f066718a9400a83507f4ad
shi-changyu123 <1163481801@qq.com>
2026-06-03 11:00:36 +0800

    管理端初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+import LoginView from '../views/login/index.vue'
+import LayoutView from '../views/layout/index.vue'
+
+const router = createRouter(
+    {
+        history: createWebHistory(),
+        routes: [{
+            path: '/',
+            name: '',
+            component: LayoutView,
+            // redirect: '/login',
+            children: [
+            ]
+        },
+//与上面子路由的父路由同级
+            {path: '/login', name: 'login', component: LoginView}
+        ]
+    }
+)
+
 
 export default router
